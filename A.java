3ae95ea9aa14d8c082b7f701bc14c16324b97904
Rakshapatel16897 <50352621+Rakshapatel16897@users.noreply.github.com>
2019-05-18 14:28:21 +0530

    Update A.java

--- A.java
+++ A.java
@@ -6,6 +6,8 @@ System.out.println("hello world");
 System.out.println("hello world..i am Rahul");
   
 System.out.println("update");
-System.out.println("New Updates");
+System.out.println("New Updates")
+  System.out.println("update1");;
+  System.out.println("update2");
 }
 }
